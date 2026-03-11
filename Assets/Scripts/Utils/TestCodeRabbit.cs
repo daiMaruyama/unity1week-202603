fd8909b34d7b1f8ce4b9e6431f8140504578131f
mkgi16cd <mkgi16cd@gmail.com>
2026-03-12 02:00:15 +0900

    test: add dummy script for CodeRabbit check

---
+++ Assets/Scripts/Utils/TestCodeRabbit.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Utils
+{
+    public class TestCodeRabbit : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("CodeRabbit test");
+        }
+    }
+}
